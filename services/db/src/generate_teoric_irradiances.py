743b2260810c59fc8065a7a8145ba1a1609d2bb0
Lucas Coelho Raupp <lucascoelhoraupp@gmail.com>
2024-10-07 11:11:45 -0300

    feat(power): Quando não há sensores disponíveis, então as irradiâncias teóricas vão para 0

--- services/db/src/generate_teoric_irradiances.py
+++ services/db/src/generate_teoric_irradiances.py
@@ -19,19 +19,29 @@ def get_period_irradiances(
         :, (classification_period == "Disponível").all()
     ]
 
-    gti_period = gti_period[classification_period.columns]
+    if classification_period.empty:
+        return pd.DataFrame(
+            {
+                "GTI teórica máxima": 0,
+                "GTI teórica média": 0,
+                "GTI teórica mediana": 0,
+            },
+            index=gti_period.index,
+        )
+    else:
+        gti_period = gti_period[classification_period.columns]
 
-    max_irradiances = gti_period.max(axis=1)
-    mean_irradiance = gti_period.mean(axis=1)
-    median_irradiance = gti_period.median(axis=1)
+        max_irradiances = gti_period.max(axis=1)
+        mean_irradiance = gti_period.mean(axis=1)
+        median_irradiance = gti_period.median(axis=1)
 
-    return pd.DataFrame(
-        {
-            "GTI teórica máxima": max_irradiances,
-            "GTI teórica média": mean_irradiance,
-            "GTI teórica mediana": median_irradiance,
-        }
-    )
+        return pd.DataFrame(
+            {
+                "GTI teórica máxima": max_irradiances,
+                "GTI teórica média": mean_irradiance,
+                "GTI teórica mediana": median_irradiance,
+            }
+        )
 
 
 def process_day(
@@ -86,39 +96,39 @@ def generate_teoric_irradiances(solar_plant: str) -> None:
     )
 
 
-def generate_teoric_irradiances(solar_plant: str) -> None:
-    def process_irradiances(
-        gti_data: pd.DataFrame,
-        classification: pd.DataFrame,
-        path: str,
-        date_range: pd.DatetimeIndex,
-    ) -> None:
-        teoric_irradiances_list = Parallel(n_jobs=-1)(
-            delayed(process_day)(gti_data, classification, date) for date in date_range
-        )
-        pd.concat(teoric_irradiances_list).to_parquet(path)
-
-    gti_avg = pd.read_parquet(PLANTS_PARAM[solar_plant]["gti_avg"])
-    gti_original = pd.read_parquet(PLANTS_PARAM[solar_plant]["gti_original"])
-    classification = pd.read_parquet(PLANTS_PARAM[solar_plant]["classification"]).drop(
-        columns=["GHI"]
+def process_irradiances(
+    gti_data: pd.DataFrame,
+    classification: pd.DataFrame,
+    path: str,
+    date_range: pd.DatetimeIndex,
+) -> None:
+    teoric_irradiances_list = Parallel(n_jobs=-1)(
+        delayed(process_day)(gti_data, classification, date) for date in date_range
     )
+
+    pd.concat(teoric_irradiances_list).to_parquet(path)
+
+
+def generate_teoric_irradiances(solar_plant: str) -> None:
+    gti_avg = pd.read_parquet(PLANTS_PARAM[solar_plant]["datawarehouse"]["gti_avg"])
+    gti_original = pd.read_parquet(
+        PLANTS_PARAM[solar_plant]["datawarehouse"]["gti_original"]
+    )
+    classification = pd.read_parquet(
+        PLANTS_PARAM[solar_plant]["datawarehouse"]["classification"]
+    ).drop(columns=["GHI"])
 
     date_range = pd.date_range(gti_avg.index.min(), gti_avg.index.max(), freq="D")
 
     process_irradiances(
         gti_avg,
         classification,
-        PLANTS_PARAM[solar_plant]["teoric_irradiances_avg"],
+        PLANTS_PARAM[solar_plant]["datawarehouse"]["teoric_irradiances_avg"],
         date_range,
     )
     process_irradiances(
         gti_original,
         classification,
-        PLANTS_PARAM[solar_plant]["teoric_irradiances_original"],
+        PLANTS_PARAM[solar_plant]["datawarehouse"]["teoric_irradiances_original"],
         date_range,
     )
-
-
-if __name__ == "__main__":
-    generate_teoric_irradiances("Apolo")
