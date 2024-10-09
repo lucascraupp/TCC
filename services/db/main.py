50e58184ba22e52e68e137883d68d957814a3c3a
Lucas Coelho Raupp <lucascoelhoraupp@gmail.com>
2024-10-09 17:51:08 -0300

    feat(power): Adicionando o cálculo da potência teórica gerada por trackers travados

--- services/db/main.py
+++ services/db/main.py
@@ -3,6 +3,7 @@ import json
 from src.generate_classification import generate_classification
 from src.generate_clearsky import generate_clearsky
 from src.generate_gti_ghi_ca import generate_gti_ghi_ca
+from src.generate_stopped_trackers_power import generate_stopped_trackers_power
 from src.generate_teoric_irradiance import generate_teoric_irradiance
 from src.generate_teoric_power import generate_teoric_power
 from src.generate_wind_speed_amb_temp import generate_wind_speed_amb_temp
@@ -13,26 +14,29 @@ if __name__ == "__main__":
     for solar_plant in PLANTS_PARAM.keys():
         print(f"\nPopulando os dados da usina {solar_plant}...\n")
 
-        print("\nPopulando os dados com média móvel...\n")
+        print("Populando os dados de GTI, GHI e potência CA com média móvel...")
         generate_gti_ghi_ca(solar_plant, True)
 
-        print("\nPopulando os dados sem média móvel...\n")
+        print("Populando os dados de GTI, GHI e potência CA sem média móvel...")
         generate_gti_ghi_ca(solar_plant, False)
 
-        print("\nPopulando os dados de céu limpo...\n")
-        generate_clearsky(solar_plant)
-
-        print("\nPopulando a classificação...\n")
-        generate_classification(solar_plant)
-
-        print("\nPopulando a irradância teórica...\n")
-        generate_teoric_irradiance(solar_plant)
-
-        print("\nPouplando a velocidade do vento e temperatura ambiente...\n")
+        print("Pouplando a velocidade do vento e temperatura ambiente...")
         generate_wind_speed_amb_temp(solar_plant)
 
-        print("\nPopulando a potência teórica com média móvel...\n")
+        print("Populando os dados de céu limpo...")
+        generate_clearsky(solar_plant)
+
+        print("Populando a classificação...")
+        generate_classification(solar_plant)
+
+        print("Populando a irradância teórica...")
+        generate_teoric_irradiance(solar_plant)
+
+        print("Populando a potência teórica com média móvel...")
         generate_teoric_power(solar_plant, True)
 
-        print("\nPopulando a potência teórica sem média móvel...\n")
+        print("Populando a potência teórica sem média móvel...")
         generate_teoric_power(solar_plant, False)
+
+        print("Populando a potência dos trackers parados...")
+        generate_stopped_trackers_power(solar_plant)
