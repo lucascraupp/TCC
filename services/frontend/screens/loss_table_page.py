3d23e6c92a569b90f4bd2d528f3126406bd10658
Lucas Coelho Raupp <lucascoelhoraupp@gmail.com>
2024-10-23 16:21:08 -0300

    feat(generate_regression): Separando a tabela de perdas por intervalo

--- services/frontend/screens/loss_table_page.py
+++ services/frontend/screens/loss_table_page.py
@@ -8,12 +8,23 @@ import streamlit as st
 PLANTS_PARAM = json.load(open("resources/solar_plants.json"))
 
 
+def get_angle() -> None:
+    angle = st.session_state.angle
+    st.session_state.angle_table = st.session_state.loss_table[
+        st.session_state.loss_table["Angulação (°)"] == angle
+    ]
+
+
 def get_data() -> None:
     solar_plant = st.session_state.solar_plant
 
     st.session_state.loss_table = pd.read_parquet(
         PLANTS_PARAM[solar_plant]["datawarehouse"]["loss_table"]
     )
+
+    st.session_state.angle_list = st.session_state.loss_table["Angulação (°)"].unique()
+
+    get_angle()
 
 
 def start_page() -> None:
@@ -37,23 +48,35 @@ def start_page() -> None:
     if st.session_state.first_start:
         st.session_state.plants_list = list(PLANTS_PARAM.keys())
         st.session_state.solar_plant = st.session_state.plants_list[0]
+        st.session_state.angle = 0
 
         st.session_state.first_start = False
 
         get_data()
 
 
-# - MARK: -- Barra lateral
 def header() -> None:
-    st.selectbox(
-        "Selecione a usina",
-        st.session_state.plants_list,
-        key="solar_plant",
-        on_change=get_data,
-    )
+    col = st.columns(2)
+
+    with col[0]:
+        st.selectbox(
+            "Selecione a usina",
+            st.session_state.plants_list,
+            key="solar_plant",
+            on_change=get_data,
+        )
+    with col[1]:
+        st.selectbox(
+            "Selecione o ângulo",
+            st.session_state.angle_list,
+            key="angle",
+            on_change=get_angle,
+        )
 
 
-def plot_loss_chart() -> None:
+def plot_loss_per_angle_bar() -> None:
+    st.title("Comportamento das perdas para diferentes CSI's")
+
     loss_table = st.session_state.loss_table
 
     csi_upper_90 = loss_table[loss_table["CSI"] > 0.9]
@@ -105,7 +128,42 @@ def plot_loss_chart() -> None:
         i += 1 if i == 0 else -1
 
 
+def plot_loss_per_csi_scatter() -> None:
+    loss_table = st.session_state.angle_table
+
+    step = 0.1
+
+    csi_range = np.arange(0, 1 - step, step)
+
+    table_list = [
+        loss_table[(loss_table["CSI"] >= csi) & (loss_table["CSI"] < csi + 0.1)]
+        for csi in csi_range
+    ]
+
+    # Tratamento especial para o último intervalo
+    last_interval = loss_table[
+        (loss_table["CSI"] >= 1 - step) & (loss_table["CSI"] <= 1.0)
+    ]
+    table_list.append(last_interval)
+
+    csi_table = pd.DataFrame(
+        {
+            "Intervalo CSI": [
+                f"[{csi:.2f}, {csi + step - 0.01:.2f}]" for csi in csi_range
+            ]
+            + [f"[{1 - step:.2f}, 1.00]"],
+            "Número de Dias": [len(table) for table in table_list],
+            "Perda Mínima (%)": [table["Perda (%)"].min() for table in table_list],
+            "Perda Média (%)": [table["Perda (%)"].mean() for table in table_list],
+            "Perda Máxima (%)": [table["Perda (%)"].max() for table in table_list],
+        }
+    )
+
+    st.write(csi_table)
+
+
 def loss_table_page() -> None:
     start_page()
     header()
-    plot_loss_chart()
+    plot_loss_per_angle_bar()
+    plot_loss_per_csi_scatter()
