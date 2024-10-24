02706a1283f47aeba215b89e9c64338afbfbcb33
Lucas Coelho Raupp <lucascoelhoraupp@gmail.com>
2024-10-24 10:57:19 -0300

    feat(generate_regression): Geração dos gráficos de perda por intervalo de CSI

--- services/frontend/screens/loss_table_page.py
+++ services/frontend/screens/loss_table_page.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 import numpy as np
 import pandas as pd
@@ -12,7 +13,7 @@ def get_angle() -> None:
     angle = st.session_state.angle
     st.session_state.angle_table = st.session_state.loss_table[
         st.session_state.loss_table["Angulação (°)"] == angle
-    ]
+    ].reset_index(drop=True)
 
 
 def get_data() -> None:
@@ -128,38 +129,143 @@ def plot_loss_per_angle_bar() -> None:
         i += 1 if i == 0 else -1
 
 
-def plot_loss_per_csi_scatter() -> None:
-    loss_table = st.session_state.angle_table
-
-    step = 0.1
-
+def calculate_interval(loss_table: pd.DataFrame, step: float) -> pd.DataFrame:
     csi_range = np.arange(0, 1 - step, step)
 
     table_list = [
-        loss_table[(loss_table["CSI"] >= csi) & (loss_table["CSI"] < csi + 0.1)]
+        loss_table[
+            (loss_table["CSI"] >= round(csi, 2))
+            & (loss_table["CSI"] < round(csi + step, 2))
+        ]
         for csi in csi_range
     ]
 
     # Tratamento especial para o último intervalo
     last_interval = loss_table[
-        (loss_table["CSI"] >= 1 - step) & (loss_table["CSI"] <= 1.0)
+        (loss_table["CSI"] >= 1 - step) & (loss_table["CSI"] <= 1)
     ]
     table_list.append(last_interval)
 
-    csi_table = pd.DataFrame(
+    return pd.DataFrame(
         {
-            "Intervalo CSI": [
-                f"[{csi:.2f}, {csi + step - 0.01:.2f}]" for csi in csi_range
-            ]
+            "Intervalo CSI": [f"[{csi:.2f}, {csi + step:.2f})" for csi in csi_range]
             + [f"[{1 - step:.2f}, 1.00]"],
-            "Número de Dias": [len(table) for table in table_list],
-            "Perda Mínima (%)": [table["Perda (%)"].min() for table in table_list],
-            "Perda Média (%)": [table["Perda (%)"].mean() for table in table_list],
-            "Perda Máxima (%)": [table["Perda (%)"].max() for table in table_list],
+            "Número de dias": [len(table) for table in table_list],
+            "Perda mínima (%)": [table["Perda (%)"].min() for table in table_list],
+            "Perda média (%)": [
+                round(table["Perda (%)"].mean(), 2) for table in table_list
+            ],
+            "Perda máxima (%)": [table["Perda (%)"].max() for table in table_list],
         }
     )
 
-    st.write(csi_table)
+
+def calculcate_quantile(loss_table: pd.DataFrame, quantile: float) -> pd.DataFrame:
+    table_by_csi = loss_table.sort_values(by="CSI").reset_index(drop=True)
+
+    length = math.ceil(len(table_by_csi) * quantile)
+
+    quant_list = []
+    begin = 0
+    for end in range(length, len(table_by_csi) + length, length):
+        table = table_by_csi.loc[begin : end - 1]
+
+        quant_list.append(
+            pd.DataFrame(
+                {
+                    "Intervalo CSI": [
+                        f"[{table["CSI"].min():.2f}, {table["CSI"].max():.2f}]"
+                    ],
+                    "Número de dias": len(table),
+                    "Perda mínima (%)": table["Perda (%)"].min(),
+                    "Perda média (%)": round(table["Perda (%)"].mean(), 2),
+                    "Perda máxima (%)": table["Perda (%)"].max(),
+                }
+            )
+        )
+
+        begin = end
+
+    return pd.concat(quant_list, ignore_index=True)
+
+
+def plot_loss_per_csi_scatter() -> None:
+    st.title("Comportamento das perdas para diferentes intervalos de CSI")
+
+    loss_table = st.session_state.angle_table
+
+    step = 0.1
+    quantile = 0.1
+
+    interval_table = calculate_interval(loss_table, step)
+    quantile_table = calculcate_quantile(loss_table, quantile)
+
+    fig = go.Figure(
+        data=[
+            go.Scatter(
+                x=interval_table["Intervalo CSI"],
+                y=interval_table["Perda média (%)"],
+                mode="markers",
+                hovertemplate=(
+                    "Intervalo CSI: %{x}<br>"
+                    "Número de dias: %{customdata[0]}<br>"
+                    "Perda mínima: %{customdata[1]}%<br>"
+                    "Perda média: %{y}%<extra></extra><br>"
+                    "Perda máxima: %{customdata[2]}%<br>"
+                ),
+                customdata=np.hstack(
+                    [
+                        interval_table[
+                            ["Número de dias", "Perda mínima (%)", "Perda máxima (%)"]
+                        ]
+                    ]
+                ),
+            )
+        ]
+    )
+
+    fig2 = go.Figure(
+        data=[
+            go.Scatter(
+                x=quantile_table["Intervalo CSI"],
+                y=quantile_table["Perda média (%)"],
+                mode="markers",
+                hovertemplate=(
+                    "Intervalo CSI: %{x}<br>"
+                    "Número de dias: %{customdata[0]}<br>"
+                    "Perda mínima: %{customdata[1]}%<br>"
+                    "Perda média: %{y}%<extra></extra><br>"
+                    "Perda máxima: %{customdata[2]}%<br>"
+                ),
+                customdata=np.hstack(
+                    [
+                        quantile_table[
+                            ["Número de dias", "Perda mínima (%)", "Perda máxima (%)"]
+                        ]
+                    ]
+                ),
+            )
+        ]
+    )
+
+    fig.update_layout(
+        xaxis_title="Intervalo CSI",
+        yaxis_title="Perda média (%)",
+        title=f"Perda por intervalo de {step}",
+    )
+
+    fig2.update_layout(
+        xaxis_title="Intervalo CSI",
+        yaxis_title="Perda média (%)",
+        title=f"Perda por quantil de {quantile}",
+    )
+
+    col = st.columns(2)
+
+    with col[0]:
+        st.plotly_chart(fig, use_container_width=True)
+    with col[1]:
+        st.plotly_chart(fig2, use_container_width=True)
 
 
 def loss_table_page() -> None:
